25c7abbe271cc948e89159dd7fdb3f6ecb8911e2
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-02-27 17:52:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include_once("index.html"); ?>
-<?php include_once("blog.html"); ?>
+<!-- <?php include_once("blog.html"); ?>
 <?php include_once("contact.html"); ?>
 <?php include_once("intro.html"); ?>
-<?php include_once("services.html"); ?>
+<?php include_once("services.html"); ?> -->
